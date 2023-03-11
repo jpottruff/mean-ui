3bd32cb2e691bf3e26b899d1de5607b0febe8dc5
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-10 19:27:22 -0500

    file structure mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
