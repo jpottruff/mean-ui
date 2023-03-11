dbfbd534eb352451ca2cb939c20e3a6160d44f1a
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-11 09:22:47 -0500

    adds clear form

--- src/app/components/posts/post-create/post-create.component.ts
+++ src/app/components/posts/post-create/post-create.component.ts
@@ -16,7 +16,8 @@ export class PostCreateComponent {
       return;
     }
 
-    this.postsService.addPost(form.value.title, form.value.content)    
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 
 }
