cbab7f6a52877941461d26bdbb4cfd16b940de8a
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-12 19:25:49 -0400

    map _id to id on get

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { map, Observable, Subject } from 'rxjs';
 import { Post } from '../models/post.interface';
 
 @Injectable({
@@ -17,9 +17,12 @@ export class PostsService {
   constructor(private readonly http: HttpClient) { }
 
   getPosts(): Post[] {
-    this.http.get<{ message: string, posts: Post[]}>(`${this.SERVER_BASE}/api/posts`)
-      .subscribe(res => {
-        this.posts = res.posts; 
+    this.http.get<{ message: string, posts: any[]}>(`${this.SERVER_BASE}/api/posts`)
+      .pipe(map(res => {
+        return res.posts.map(post => this.convertFetchedPost(post))
+      }))
+      .subscribe(posts => {
+        this.posts = posts; 
         this.postsUpdated.next([...this.posts])
       })
     return [...this.posts];
@@ -37,5 +40,14 @@ export class PostsService {
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
+  }
+
+  /** Converts a post retrieved from the backend to the appropriate form */
+  private convertFetchedPost(post: any): Post {
+    return {
+      id: post._id,
+      title: post.title,
+      content: post.content
+    }
   }
 }
