863439402fab06634754dc05609bba1afef79a63
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-18 10:24:44 -0400

    add image to post form

--- src/app/components/posts/post-create/post-create.component.ts
+++ src/app/components/posts/post-create/post-create.component.ts
@@ -27,6 +27,9 @@ export class PostCreateComponent implements OnInit {
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
+      }),
       content: new FormControl(null, {
         validators: [Validators.required]
       })
@@ -50,7 +53,12 @@ export class PostCreateComponent implements OnInit {
         this.mode = EditMode.CREATE
       }
     });
+  }
 
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
   }
 
   onSavePost() {
