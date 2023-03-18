d536532245cdd3cadabe5d8e467b485f835f7a2d
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-18 12:01:46 -0400

    add mime type validator and hook up to post form

--- src/app/components/posts/post-create/post-create.component.ts
+++ src/app/components/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from 'src/app/models/post.interface';
 import { PostsService } from 'src/app/services/posts.service';
+import { mimeType } from 'src/app/validators/mime-type.validator';
 
 export enum EditMode {
   CREATE = 'CREATE',
@@ -29,7 +30,8 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)]
       }),
       image: new FormControl(null, {
-        validators: [Validators.required]
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
       }),
       content: new FormControl(null, {
         validators: [Validators.required]
---
+++ src/app/validators/mime-type.validator.ts
@@ -0,0 +1,50 @@
+import { AbstractControl } from "@angular/forms";
+import { Observable, Observer } from "rxjs";
+
+export const mimeType = (control: AbstractControl)
+: Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
+    const file = control.value as File;
+    const fileReader = new FileReader();
+    const fileReader$ = new Observable((observer: Observer<{[key: string]: any}>) => {
+        fileReader.addEventListener('loadend', () => {
+            // * PUT TOGETHER FILE MAGIC NUMBER (https://stackoverflow.com/a/75516030)
+            const arr = new Uint8Array(fileReader.result as ArrayBuffer).subarray(0, 4);
+            let header = ''; 
+            for (const bytes of arr) {
+                header += bytes.toString(16);
+            }
+
+            // * VALIDATE FILE MAGIC NUMBER (https://gist.github.com/leommoore/f9e57ba2aa4bf197ebc5)
+            let isValid = false;
+            switch (header) {
+                case "89504e47": // png
+                  isValid = true;
+                  break;
+                case "ffd8ffe0": // jpeg (jfif)
+                case "ffd8ffe1": // jpeg (exif)
+                case "ffd8ffe2": // jpeg (canon)
+                case "ffd8ffe3": // jpeg (samsung)
+                case "ffd8ffe8": // jpeg (spiff)
+                  isValid = true;
+                  break;
+                default:
+                  isValid = false; // Or you can use the blob.type as fallback
+                  break;
+            }
+
+            // * ANGULAR VALIDATION RESULTS
+            // `null` if valid; set of validation errors if not (https://angular.io/guide/form-validation#validator-functions)
+            if (isValid) {
+                observer.next(null);
+            } else {
+                observer.next({ invalidMimeType: true });
+            }
+            observer.complete();
+        });
+
+        // * READ FILE TO EXECUTE EVENT LISTENER
+        fileReader.readAsArrayBuffer(file);  
+    });
+
+    return fileReader$;
+}
