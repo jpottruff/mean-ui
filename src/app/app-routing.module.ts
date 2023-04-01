4a2e18ecacaadd0dcbc12a28f8358dabd5e5b8f5
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-01 10:38:27 -0400

    add route guard and route guard migration todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { PostListComponent } from './components/posts/post-list/post-list.compon
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path:  '', component: PostListComponent },
-  { path:  'create', component: PostCreateComponent },
-  { path:  'edit/:id', component: PostCreateComponent },
+  { path:  'create', component: PostCreateComponent, canActivate: [ AuthGuard ] },
+  { path:  'edit/:id', component: PostCreateComponent, canActivate: [ AuthGuard ] },
   { path:  'login', component: LoginComponent, },
   { path:  'signup', component: SignUpComponent, },
 ]
@@ -17,6 +18,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ AuthGuard ]
 })
 export class AppRoutingModule { }
